09ad6d499ae8fb72fb473696eaea113bbd208b9c
Hasitha7 <hasithakumarasiri98@gmail.com>
2026-05-17 11:03:11 +0530

    refresh issue fix4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { Layout } from './components/Layout';
 
@@ -29,40 +29,38 @@ import LabourerDashboard, {
 const App = () => {
   return (
     <AppProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            {/* Admin Routes */}
-            <Route path="/admin/dashboard" element={<AdminDashboard />} />
-            <Route path="/admin/clients" element={<ClientsPage />} />
-            <Route path="/admin/labourers" element={<LaboursPage />} />
-            <Route path="/admin/assignments" element={<AssignmentsPage />} />
-            <Route path="/admin/attendance" element={<AttendancePage />} />
-            <Route path="/admin/payroll" element={<PayrollPage />} />
-            <Route path="/admin/ratings" element={<RatingsPage />} />
-            <Route path="/admin/invoices" element={<InvoicesPage />} />
-            <Route path="/admin/reports" element={<ReportsPage />} />
-            <Route path="/admin/settings" element={<SettingsPage />} />
+      <Layout>
+        <Routes>
+          {/* Admin Routes */}
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/clients" element={<ClientsPage />} />
+          <Route path="/admin/labourers" element={<LaboursPage />} />
+          <Route path="/admin/assignments" element={<AssignmentsPage />} />
+          <Route path="/admin/attendance" element={<AttendancePage />} />
+          <Route path="/admin/payroll" element={<PayrollPage />} />
+          <Route path="/admin/ratings" element={<RatingsPage />} />
+          <Route path="/admin/invoices" element={<InvoicesPage />} />
+          <Route path="/admin/reports" element={<ReportsPage />} />
+          <Route path="/admin/settings" element={<SettingsPage />} />
 
-            {/* Client Routes */}
-            <Route path="/client/dashboard" element={<ClientDashboard />} />
-            <Route path="/client/workers" element={<ClientWorkers />} />
-            <Route path="/client/invoices" element={<ClientInvoices />} />
-            <Route path="/client/ratings" element={<ClientRatings />} />
+          {/* Client Routes */}
+          <Route path="/client/dashboard" element={<ClientDashboard />} />
+          <Route path="/client/workers" element={<ClientWorkers />} />
+          <Route path="/client/invoices" element={<ClientInvoices />} />
+          <Route path="/client/ratings" element={<ClientRatings />} />
 
-            {/* Labourer Routes */}
-            <Route path="/labourer/dashboard" element={<LabourerDashboard />} />
-            <Route path="/labourer/assignments" element={<LabourerJobs />} />
-            <Route path="/labourer/attendance" element={<LabourerAttendance />} />
-            <Route path="/labourer/payroll" element={<LabourerPayroll />} />
-            <Route path="/labourer/ratings" element={<LabourerRatings />} />
+          {/* Labourer Routes */}
+          <Route path="/labourer/dashboard" element={<LabourerDashboard />} />
+          <Route path="/labourer/assignments" element={<LabourerJobs />} />
+          <Route path="/labourer/attendance" element={<LabourerAttendance />} />
+          <Route path="/labourer/payroll" element={<LabourerPayroll />} />
+          <Route path="/labourer/ratings" element={<LabourerRatings />} />
 
-            {/* Default Redirect */}
-            <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
-            <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
-          </Routes>
-        </Layout>
-      </Router>
+          {/* Default Redirect */}
+          <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
+          <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
+        </Routes>
+      </Layout>
     </AppProvider>
   );
 };
