465ac4f97dbe8e9e172b5f902c5800a8f9ef2799
Hasitha7 <hasithakumarasiri98@gmail.com>
2026-05-17 10:51:36 +0530

    refresh issue fix2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
